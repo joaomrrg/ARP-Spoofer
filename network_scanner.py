e689c9f85dc749586e160e4b57ba53f2e74c8938
joaomrrg <120472343+joaomrrg@users.noreply.github.com>
2023-02-04 18:50:16 +0000

    Update network_scanner.py
    Network Scanner:
    -Understanding the ARP protocol
    -Create and use ARP frames and combining them to broadcast packets
    -Sending and receiving the packets

--- network_scanner.py
+++ network_scanner.py
@@ -1,1 +1,36 @@
+#!/usr/bin/env python
+import optparse
+import scapy.all as scapy
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="ip", help="IP address to target")
+    options = parser.parse_args()[0]
+    if not options.ip:
+        print("[-] Please specify a IP address to target")
+    else:
+        return options
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list
+
+def create_table(list):
+    clients_list = []
+    for element in list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_table(result_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+options = get_arguments()
+
+if options:
+    print_table(create_table(scan(options.ip)))
